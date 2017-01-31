96a8235458fd10b3e9dcc648b5c89b9535806a48
Philipp Wördehoff <philipp.woerdehoff@flixbus.com>
2017-01-31 10:55:51 +0100

    automated backup commit

--- channel_referencing/main.go
+++ channel_referencing/main.go
@@ -8,30 +8,24 @@ import (
 func main() {
 	chan1 := make(chan string, 100)
 	chan1<-"chan1_1"
-	chan1<-"chan1_2"
-	chan1<-"chan1_3"
-	chan1<-"chan1_4"
-	chan1<-"chan1_5"
 
 	chan2 := make(chan string, 100)
 	chan2<-"chan2_1"
-	chan2<-"chan2_2"
-	chan2<-"chan2_3"
-	chan2<-"chan2_4"
-	chan2<-"chan2_5"
 
-	usedChannel := chan1
+	var usedChannel *chan string = &chan1
 
 	go func() {
 		for {
-			log.Print(<-usedChannel)
-			time.Sleep(1 * time.Second)
+			channel := *usedChannel
+			msg := <-channel
+			log.Print(msg)
+			channel <- msg
 		}
 	}()
 
 	time.Sleep(2 * time.Second)
 
-	usedChannel = chan2
+	usedChannel = &chan2
 
 	time.Sleep(3 * time.Second)
 }
